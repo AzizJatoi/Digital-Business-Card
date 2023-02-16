468cfb16f745dbb1607d859d34b295f88dfe1b0c
Aziz <emailazizalij@gmail.com>
2023-02-16 19:34:02 +0500

    final changes

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
             <img src={require('../src/images/twitter.png')} alt="twitter logo" className='footer-icons'/>
             <img src={require('../src/images/facebook.png')} alt="facebook logo" className='footer-icons'/>
             <img src={require('../src/images/instagram.png')} alt="instagram logo" className='footer-icons'/>
-            <img src={require('../src/images/linkedin.png')}alt="linkedin logo" className='footer-icons' />
+            <a href='https://www.linkedin.com/in/abdul-aziz-jatoi/'><img src={require('../src/images/linkedin.png')}alt="linkedin logo" className='footer-icons' /></a>
             <img src={require('../src/images/github.png')} alt="github logo" className='footer-icons'/>
             </footer>
         </div>
--- src/Header.js
+++ src/Header.js
@@ -9,8 +9,7 @@ function Header() {
         <h1>Aziz Jatoi</h1>
         <p className='profession'>Frontend Deveoloper</p>
         <a className='github-link' href="https://github.com/AzizJatoi">github.com/AzizJatoi</a>
-        <button><img src={require('../src/images/Mail.png')} alt="person" className="mail-icon" />
-         Email</button>
+        <p className="email"><img src={require('../src/images/Mail.png')} alt="mail" className="mail-icon" /><a href="mailto:emailazizalij@gmail.com">Email</a></p>
         </div>
       </header>
     </div>
