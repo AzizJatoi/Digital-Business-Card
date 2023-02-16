c1f1e21a429f0764c6bf076aba562b018a4d1c85
Aziz <emailazizalij@gmail.com>
2023-02-16 19:12:12 +0500

    link to github

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ function Header() {
       <div className='info'>
         <h1>Aziz Jatoi</h1>
         <p className='profession'>Frontend Deveoloper</p>
-        <p className='github-link'>github.com/AzizJatoi</p>
+        <a className='github-link' href="https://github.com/AzizJatoi">github.com/AzizJatoi</a>
         <button><img src={require('../src/images/Mail.png')} alt="person" className="mail-icon" />
          Email</button>
         </div>
